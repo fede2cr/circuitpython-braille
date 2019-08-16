9136c66527d2a8c9d20397a7ffa6915077cb7f92
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-08-16 12:30:50 -0600

    Adding HID support

--- code.py
+++ code.py
@@ -1,11 +1,18 @@
 import braille
 import time
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_circuitplayground.express import cpx
+
+keyboard = Keyboard()
+keyboard_layout = KeyboardLayoutUS(keyboard)
 
 while True:
     dots = braille.readDots()
     if dots in braille.dots_to_letter.keys():
-        print(braille.dots_to_letter[dots])
+        if cpx.switch is True:
+            print(braille.dots_to_letter[dots])
+            print(braille.dots_to_braille[dots])
+        else:
+            keyboard_layout.write(braille.dots_to_letter[dots])
     time.sleep(0.01)
-    dots = braille.readDots()
-    if dots in braille.dots_to_braille.keys():
-        print(braille.dots_to_braille[dots])
