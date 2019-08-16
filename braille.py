53420d4e91bb2919237d6eef942d8de28973e57a
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-08-16 12:13:50 -0600

    Adding to-braille, code cleanup

--- braille.py
+++ braille.py
@@ -14,46 +14,69 @@ backspace = touchio.TouchIn(board.A7)
 
 # 6 dots plus backspace
 dots_to_letter = {
-0b1000000: "A",
-0b1100000: "B",
-0b1001000: "C",
-0b1001100: "D",
-0b1000100: "E",
-0b1101000: "F",
-0b1101100: "G",
-0b1100100: "H",
-0b0101000: "I",
-0b0101100: "J",
-0b1010000: "K",
-0b1110000: "L",
-0b1011000: "M",
-0b1011100: "N",
-0b1010100: "O",
-0b1111000: "P",
-0b1111100: "Q",
-0b1110100: "R",
-0b0111000: "S",
-0b0111100: "T",
-0b1010010: "U",
-0b1110010: "V",
-0b0101110: "W",
-0b1011010: "X",
-0b1011110: "Y",
-0b1010110: "Z",
+0b1000000: "a", # ⠁
+0b1100000: "b", # ⠃
+0b1001000: "c", # ⠉
+0b1001100: "d", # ⠙
+0b1000100: "e", # ⠑
+0b1101000: "f", # ⠋
+0b1101100: "g", # ⠛
+0b1100100: "h", # ⠓
+0b0101000: "i", # ⠊
+0b0101100: "j", # ⠚
+0b1010000: "k", # ⠅
+0b1110000: "l", # ⠇
+0b1011000: "m", # ⠍
+0b1011100: "n", # ⠍
+0b1010100: "o", # ⠕
+0b1111000: "p", # ⠕
+0b1111100: "q", # ⠟
+0b1110100: "r", # ⠗
+0b0111000: "s", # ⠗
+0b0111100: "t", # ⠞
+0b1010010: "u", # ⠥
+0b1110010: "v", # ⠧
+0b0101110: "w", # ⠺
+0b1011010: "x", # ⠭
+0b1011110: "y", # ⠽
+0b1010110: "z", # ⠵
 0b0000001: "backspace",
 }
 
+# 6 dots plus backspace
+dots_to_braille = {
+0b1000000: "⠁", # a
+0b1100000: "⠃", # b
+0b1001000: "⠉", # c
+0b1001100: "⠙", # d
+0b1000100: "⠑", # e
+0b1101000: "⠋", # f
+0b1101100: "⠛", # g
+0b1100100: "⠓", # h
+0b0101000: "⠊", # i
+0b0101100: "⠚", # j
+0b1010000: "⠅", # k
+0b1110000: "⠇", # l
+0b1011000: "⠍", # m
+0b1011100: "⠍", # n
+0b1010100: "⠕", # o
+0b1111000: "⠕", # p
+0b1111100: "⠟", # q
+0b1110100: "⠗", # r
+0b0111000: "⠗", # s
+0b0111100: "⠞", # t
+0b1010010: "⠥", # u
+0b1110010: "⠧", # v
+0b0101110: "⠺", # w
+0b1011010: "⠭", # x
+0b1011110: "⠽", # y
+0b1010110: "⠵", # z
+0b0000001: "backspace",
+}
+
+
 def readDots():
     dots = int(backspace.value) | int(dot6.value) << 1  | int(dot5.value) << 2 | int(dot4.value) << 3 | int(dot3.value) << 4 | int(dot2.value) << 5 | int(dot1.value) << 6
+    while dots == 0 and dots not in dots_to_letter.keys():
+        dots = int(backspace.value) | int(dot6.value) << 1  | int(dot5.value) << 2 | int(dot4.value) << 3 | int(dot3.value) << 4 | int(dot2.value) << 5 | int(dot1.value) << 6
     return dots
-
-def readKeyboard():
-    dots = readDots()
-    while dots == 0b0:
-        time.sleep(0.1)
-        dots = readDots()
-        #print("ceros")
-    print(bin(dots))
-    print(dots_to_letter.keys())
-    print(dots_to_letter[dots])
-    time.sleep(1)
--- code.py
+++ code.py
@@ -1,7 +1,11 @@
 import braille
-
 import time
 
 while True:
-    braille.readKeyboard()
+    dots = braille.readDots()
+    if dots in braille.dots_to_letter.keys():
+        print(braille.dots_to_letter[dots])
     time.sleep(0.01)
+    dots = braille.readDots()
+    if dots in braille.dots_to_braille.keys():
+        print(braille.dots_to_braille[dots])
