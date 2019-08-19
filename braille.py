0801705d6680dcc63479eee62d0338adc20814fe
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-08-19 11:37:52 -0600

    Moving to nested dictionary, adding indicators and simple

--- braille.py
+++ braille.py
@@ -13,70 +13,45 @@ backspace = touchio.TouchIn(board.A7)
 
 
 # 6 dots plus backspace
-dots_to_letter = {
-0b1000000: "a", # ⠁
-0b1100000: "b", # ⠃
-0b1001000: "c", # ⠉
-0b1001100: "d", # ⠙
-0b1000100: "e", # ⠑
-0b1101000: "f", # ⠋
-0b1101100: "g", # ⠛
-0b1100100: "h", # ⠓
-0b0101000: "i", # ⠊
-0b0101100: "j", # ⠚
-0b1010000: "k", # ⠅
-0b1110000: "l", # ⠇
-0b1011000: "m", # ⠍
-0b1011100: "n", # ⠍
-0b1010100: "o", # ⠕
-0b1111000: "p", # ⠕
-0b1111100: "q", # ⠟
-0b1110100: "r", # ⠗
-0b0111000: "s", # ⠗
-0b0111100: "t", # ⠞
-0b1010010: "u", # ⠥
-0b1110010: "v", # ⠧
-0b0101110: "w", # ⠺
-0b1011010: "x", # ⠭
-0b1011110: "y", # ⠽
-0b1010110: "z", # ⠵
-0b0000001: "backspace",
+dots_to_alphabet = {
+0b1000000: { 'latin': "a", 'braille': '⠁'},
+0b1100000: { 'latin': "b", 'braille': '⠃'},
+0b1001000: { 'latin': "c", 'braille': '⠉'},
+0b1001100: { 'latin': "d", 'braille': '⠙'},
+0b1000100: { 'latin': "e", 'braille': '⠑'},
+0b1101000: { 'latin': "f", 'braille': '⠋'},
+0b1101100: { 'latin': "g", 'braille': '⠛'},
+0b1100100: { 'latin': "h", 'braille': '⠓'},
+0b0101000: { 'latin': "i", 'braille': '⠊'},
+0b0101100: { 'latin': "j", 'braille': '⠚'},
+0b1010000: { 'latin': "k", 'braille': '⠅'},
+0b1110000: { 'latin': "l", 'braille': '⠇'},
+0b1011000: { 'latin': "m", 'braille': '⠍'},
+0b1011100: { 'latin': "n", 'braille': '⠍'},
+0b1010100: { 'latin': "o", 'braille': '⠕'},
+0b1111000: { 'latin': "p", 'braille': '⠕'},
+0b1111100: { 'latin': "q", 'braille': '⠟'},
+0b1110100: { 'latin': "r", 'braille': '⠗'},
+0b0111000: { 'latin': "s", 'braille': '⠗'},
+0b0111100: { 'latin': "t", 'braille': '⠞'},
+0b1010010: { 'latin': "u", 'braille': '⠥'},
+0b1110010: { 'latin': "v", 'braille': '⠧'},
+0b0101110: { 'latin': "w", 'braille': '⠺'},
+0b1011010: { 'latin': "x", 'braille': '⠭'},
+0b1011110: { 'latin': "y", 'braille': '⠽'},
+0b1010110: { 'latin': "z", 'braille': '⠵'},
+# Simples
+0b0010000: { 'latin': "'", 'braille': '⠄'},
+0b0110000: { 'latin': ";", 'braille': '⠆'},
+0b0100100: { 'latin': ":", 'braille': '⠒'},
+# Special and indicators
+0b0000010: { 'latin': 'capital', 'braille': '⠠'},
+0b0011110: { 'latin': 'number', 'braille': '⠼'},
+0b0000001: { 'latin': "backspace", 'braille': ''},
 }
-
-# 6 dots plus backspace
-dots_to_braille = {
-0b1000000: "⠁", # a
-0b1100000: "⠃", # b
-0b1001000: "⠉", # c
-0b1001100: "⠙", # d
-0b1000100: "⠑", # e
-0b1101000: "⠋", # f
-0b1101100: "⠛", # g
-0b1100100: "⠓", # h
-0b0101000: "⠊", # i
-0b0101100: "⠚", # j
-0b1010000: "⠅", # k
-0b1110000: "⠇", # l
-0b1011000: "⠍", # m
-0b1011100: "⠍", # n
-0b1010100: "⠕", # o
-0b1111000: "⠕", # p
-0b1111100: "⠟", # q
-0b1110100: "⠗", # r
-0b0111000: "⠗", # s
-0b0111100: "⠞", # t
-0b1010010: "⠥", # u
-0b1110010: "⠧", # v
-0b0101110: "⠺", # w
-0b1011010: "⠭", # x
-0b1011110: "⠽", # y
-0b1010110: "⠵", # z
-0b0000001: "backspace",
-}
-
 
 def readDots():
     dots = int(backspace.value) | int(dot6.value) << 1  | int(dot5.value) << 2 | int(dot4.value) << 3 | int(dot3.value) << 4 | int(dot2.value) << 5 | int(dot1.value) << 6
-    while dots == 0 and dots not in dots_to_letter.keys():
+    while dots == 0 and dots not in dots_to_alphabet.keys():
         dots = int(backspace.value) | int(dot6.value) << 1  | int(dot5.value) << 2 | int(dot4.value) << 3 | int(dot3.value) << 4 | int(dot2.value) << 5 | int(dot1.value) << 6
     return dots
--- code.py
+++ code.py
@@ -9,10 +9,10 @@ keyboard_layout = KeyboardLayoutUS(keyboard)
 
 while True:
     dots = braille.readDots()
-    if dots in braille.dots_to_letter.keys():
+    if dots in braille.dots_to_alphabet.keys():
         if cpx.switch is True:
-            print(braille.dots_to_letter[dots])
-            print(braille.dots_to_braille[dots])
+            print(braille.dots_to_alphabet[dots]['latin'])
+            print(braille.dots_to_alphabet[dots]['braille'])
         else:
-            keyboard_layout.write(braille.dots_to_letter[dots])
+            keyboard_layout.write(braille.dots_to_alphabet[dots]['latin'])
     time.sleep(0.01)
