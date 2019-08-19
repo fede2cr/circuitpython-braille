da79ca0eb486896f38c94a9897e4da7b7a6f0ac4
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-08-19 15:56:54 -0600

    Testing debouncer, code cleanup to use cpx library

--- code.py
+++ code.py
@@ -1,18 +1,42 @@
-import braille
 import time
+from adafruit_circuitplayground.express import cpx
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_circuitplayground.express import cpx
+import alphabet
 
 keyboard = Keyboard()
 keyboard_layout = KeyboardLayoutUS(keyboard)
 
-while True:
-    dots = braille.readDots()
-    if dots in braille.alphabet.dots_to_alphabet.keys():
-        if cpx.switch is True:
-            print(braille.alphabet.dots_to_alphabet[dots]['latin'])
-            print(braille.alphabet.dots_to_alphabet[dots]['braille'])
-        else:
-            keyboard_layout.write(braille.alphabet.dots_to_alphabet[dots]['latin'])
-    time.sleep(0.01)
+def dotsToBinary():
+    binaryDots = int(cpx.touch_A7) | int(cpx.touch_A6) << 1  | int(cpx.touch_A5) << 2 | int(cpx.touch_A4) << 3 | int(cpx.touch_A3) << 4 | int(cpx.touch_A2) << 5 | int(cpx.touch_A1) << 6
+#    print(bin(binaryDots))
+    time.sleep(0.1)
+    return binaryDots
+
+def readDots():
+    while True:
+        dots = dotsToBinary()
+        if (dots != 0) or (dots not in alphabet.dots_to_alphabet.keys() ):
+            returnDots = dots
+            time.sleep(0.01)
+        dots = dotsToBinary()
+        if dots == returnDots:
+            while dotsToBinary() != 0:
+                time.sleep(0.01)
+            return dots
+
+
+def keyboardLoop():
+    while True:
+        dots = readDots()
+        if dots in alphabet.dots_to_alphabet.keys():
+            if cpx.switch is True:
+                print(alphabet.dots_to_alphabet[dots]['latin'])
+                print(alphabet.dots_to_alphabet[dots]['braille'])
+            else:
+                keyboard_layout.write(alphabet.dots_to_alphabet[dots]['latin'])
+        time.sleep(0.01)
+
+
+keyboardLoop()
+#braille.gameLoop() # TODO, for learning using a simple game
